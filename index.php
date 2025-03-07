a60d4d964fba645461485a95a274746d735b7dc2
Adrian Noces <adriannoces1@gmail.com>
2025-03-07 09:32:41 +0000

    m

--- index.php
+++ index.php
@@ -9,42 +9,7 @@
 
     </head>
     <body>
-
-        <div class="header container-fluid text-bg-dark">
-            <div class="row">
-                <div class="col-sm-4"></div>
-                
-                <?php 
-                    $arrPage = ["Home","About me","Blogs","Skills","Contacts"];
-                    $arrRange = count($arrPage);
-                    for ($i = 0; $i < $arrRange; $i++): ?>
-
-                        <div class="col pt-sm-5 pb-sm-3">
-
-                            <form action="pages/<?php echo $arrPage[$i]?>.php">
-                                <button class="homePageBtn text-bg-dark" type="submit" name="<?php echo $arrPage[$i]?>"><?php echo $arrPage[$i]?></button>
-                            </form>
-
-                        </div>
-                        
-                <?php endfor; ?>    
-            </div>
-        </div>
-        <div></div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
